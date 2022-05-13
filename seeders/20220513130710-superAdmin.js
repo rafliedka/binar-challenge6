7bb057b0d163564a6e21d81cbceff3ac057a4b99
rafliedka <rafliedka33@gmail.com>
2022-05-13 21:50:30 +0700

    chage seeder data

--- seeders/20220513130710-superAdmin.js
+++ seeders/20220513130710-superAdmin.js
@@ -4,8 +4,8 @@ const bcrypt = require('bcryptjs');
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('users', [{
-      name: "ucok",
-      email: "ucon@gmail.com",
+      name: "superadmin",
+      email: "superadmin@gmail.com",
       password: bcrypt.hashSync("ucok", 10),
       type: "superadmin",
       createdAt: new Date(),
